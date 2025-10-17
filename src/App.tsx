d51caad081bcccc3f781e475c08bbfd9c3e4bb4e
hilubabz <utsargam44@gmail.com>
2025-10-17 08:32:47 +0545

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -18,172 +18,73 @@ import Form from "./Form";
 import { parseAsInteger, useQueryState } from "nuqs";
 import Data from "./Data";
 import ExtensionManager from "./ExtensionManager";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+
+const pages = [
+  { id: 1, name: "Guess_The_Number", path: "/" },
+  { id: 2, name: "Holy_Grail", path: "/holyGrail" },
+  { id: 3, name: "Progress_Bar", path: "/progressBar" },
+  { id: 4, name: "Contact_Form", path: "/contactForm" },
+  { id: 5, name: "Like_Button", path: "/likeButton" },
+  { id: 6, name: "Loading_Skeleton", path: "/loadingSkeleton" },
+  { id: 7, name: "Toast", path: "/toast" },
+  { id: 8, name: "OTP", path: "/OTP" },
+  { id: 9, name: "Data_Table", path: "/dataTable" },
+  { id: 10, name: "Tabs", path: "/tabs" },
+  { id: 11, name: "Weather", path: "/weather" },
+  { id: 12, name: "Character_Counter", path: "/characterCounter" },
+  { id: 13, name: "To_Do_List", path: "/toDoList" },
+  { id: 14, name: "React_Hook_Form", path: "/rhf" },
+  { id: 15, name: "React_Hook_Form_Data", path: "/data" },
+  { id: 16, name: "Extension_Manager", path: "/extensionManager" },
+];
 
 function App() {
   const [page, setPage] = useQueryState("page", parseAsInteger.withDefault(1));
+  const [open, setOpen] = useState<boolean>(true);
   return (
     <>
-      <nav className={"flex space-x-4 flex-wrap pb-1 bg-gray-100 z-20"}>
-        <Link
-          to={"/"}
-          className={`text-lg ${page === 1 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(1)}
-        >
-          Guess_The_Number
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 1 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/holyGrail"}
-          className={`text-lg ${page === 2 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(2)}
-        >
-          Holy_Grail
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 2 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/progressBar"}
-          className={`text-lg ${page === 3 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(3)}
-        >
-          Progress_Bar
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 3 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/contactForm"}
-          className={`text-lg ${page === 4 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(4)}
-        >
-          Contact_Form
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 4 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/likeButton"}
-          className={`text-lg ${page === 5 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(5)}
-        >
-          Like_Button
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 5 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/loadingSkeleton"}
-          className={`text-lg ${page === 6 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(6)}
-        >
-          Loading_Skeleton
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 6 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/toast"}
-          className={`text-lg ${page === 7 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(7)}
-        >
-          Toast
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 7 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/OTP"}
-          className={`text-lg ${page === 8 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(8)}
-        >
-          OTP
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 8 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/dataTable"}
-          className={`text-lg ${page === 9 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(9)}
-        >
-          Data_Table
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 9 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/tabs"}
-          className={`text-lg ${page === 10 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(10)}
-        >
-          Tabs
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 10 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/weather"}
-          className={`text-lg ${page === 11 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(11)}
-        >
-          Weather
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 11 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/characterCounter"}
-          className={`text-lg ${page === 12 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(12)}
-        >
-          Character_Counter
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 12 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/toDoList"}
-          className={`text-lg ${page === 13 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(13)}
-        >
-          To_Do_List
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 13 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/rhf"}
-          className={`text-lg ${page === 14 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(14)}
-        >
-          React_Hook_Form
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 14 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/data"}
-          className={`text-lg ${page === 15 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(15)}
-        >
-          React_Hook_Form_Data
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 15 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
-        <Link
-          to={"/extensionManager"}
-          className={`text-lg ${page === 16 ? "font-semibold" : "text-gray-500"} relative transition-all ease-in-out duration-500`}
-          onClick={() => setPage(16)}
-        >
-          Extension_Manager
-          <div
-            className={`absolute left-0 bottom-0 h-1 rounded-xl bg-black ${page === 16 ? "w-full" : "w-0"} transition-all ease-in-out duration-500`}
-          ></div>
-        </Link>
+      <div className="fixed top-5 left-5 z-30 pl-5 text-black text-2xl flex items-center space-x-3">
+        {!open ? (
+          <CiMenuBurger onClick={() => setOpen(true)} />
+        ) : (
+          <>
+            <IoMdClose onClick={() => setOpen(false)} />
+            <div>Mini Projects</div>
+          </>
+        )}
+      </div>
+      <nav
+        className={`flex flex-col space-y-4 overflow-auto pb-1 bg-gray-100 z-20 fixed left-0 top-0 h-[100vh] w-[70vw] md:w-[30vw] lg:w-[20vw] pt-15 pl-5 ${open ? "" : "lg:-translate-x-[20vw] -translate-x-[70vw] md:-translate-x-[30vw]"} transition-all ease-in-out duration-500`}
+      >
+        <div className="flex-1 overflow-y-auto pl-5 pr-3 py-4 space-y-3">
+          {pages.map((p) => (
+            <Link
+              key={p.id}
+              to={p.path}
+              onClick={() => {
+                setPage(p.id);
+                setOpen(false);
+              }}
+              className={`block text-base font-medium ${
+                page === p.id
+                  ? "text-black font-semibold"
+                  : "text-gray-500 hover:text-gray-800"
+              } transition-all duration-300 ease-in-out`}
+            >
+              <div className={`inline-block relative`}>
+                {p.name}
+                <div
+                  className={`absolute left-0 bottom-0 h-[2px] rounded-xl bg-black transition-all duration-500 ${
+                    page === p.id ? "w-full" : "w-0"
+                  }`}
+                ></div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<GuessTheNumber />} />
